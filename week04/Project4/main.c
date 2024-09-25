18b1af2bfa4751509a7b5f0edb812f01f5d4161f
Seolhayoung <gkdud4930@naver.com>
2024-09-25 14:31:19 +0900

    week04

---
+++ week04/Project4/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+	float num1,num2;
+	
+	printf("enter two integers: ");
+	scanf("%f, %f",&num1,&num2);
+	
+	printf("%f / %f = %f",num1,num2,num1/num2);
+	return 0;
+}
