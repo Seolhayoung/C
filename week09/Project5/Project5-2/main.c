bcffadf613f7ab66e0f06043019a0e0af932c7ac
Seolhayoung <gkdud4930@naver.com>
2024-10-30 14:49:18 +0900

    week09

---
+++ week09/Project5/Project5-2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+static int sum=1;
+
+int main(void) {
+	int sum=0;
+	
+	printf("sum=%d\n",sum);
+	
+	return 0;
+}
