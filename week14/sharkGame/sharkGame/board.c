3b7386bbec6c6b89c2d318e8d35a94b065667047
Seolhayoung <gkdud4930@naver.com>
2024-12-08 18:01:07 +0900

    week14

---
+++ week14/sharkGame/sharkGame/board.c
@@ -0,0 +1,113 @@
+//
+//  board.c
+//  sharkGame
+//
+
+#include "board.h"
+
+// ----- EX. 3 : board ------------
+#define N_COINPOS       12
+#define MAX_COIN        4
+// ----- EX. 3 : board ------------
+// ----- EX. 5 : shark ------------
+#define MAX_SHARKSTEP   6
+#define SHARK_INITPOS   -4
+// ----- EX. 5 : shark ------------
+// ----- EX. 3 : board ------------
+static int board_coin[N_BOARD];
+static int board_status[N_BOARD]; //0 - OK, 1 - destroyed
+// ----- EX. 3 : board ------------
+
+
+static int shark_position;
+
+// ----- EX. 3 : board ------------
+void board_printBoardStatus(void)
+{
+    int i;
+    
+    printf("----------------------- BOARD STATUS -----------------------\n");
+    for (i=0;i<N_BOARD;i++)
+    {
+        printf("|");
+        if (board_status[i] == BOARDSTATUS_NOK)
+            printf("X");
+        else
+            printf("O");
+    }
+    printf("|\n");
+    printf("------------------------------------------------------------\n");
+}
+
+int board_initBoard(void)
+{
+    int i;
+    
+    //variable initialization
+    for (i=0;i<N_BOARD;i++)
+    {
+        board_status[i] = BOARDSTATUS_OK;
+        board_coin[i] = 0;
+    }
+    
+// ----- EX. 5 : shark ------------
+    shark_position = SHARK_INITPOS;
+// ----- EX. 5 : shark ------------
+
+    //coin allocation
+    for (i=0;i<N_COINPOS;i++){
+    	
+    	int pos;
+    	
+    	do {
+    		pos=rand()%N_BOARD;
+		} while (board_coin[pos]>=MAX_COIN);
+		
+		board_coin[pos]++;
+	}
+    
+    return N_COINPOS;
+}
+// ----- EX. 3 : board ------------
+
+
+// ----- EX. 5 : shark ------------
+int board_stepShark(void)
+{
+	int step = rand() % MAX_SHARKSTEP + 1; // Roll a die for shark
+    int start = shark_position + 1;       // Start position of the shark
+    int end = shark_position + step;      // End position of the shark
+    
+    if(end>=N_BOARD){
+    	
+    	end=N_BOARD-1;
+	}
+	
+	int i;
+	for (i = start; i <= end; i++) {
+        if (i >= 0 && i < N_BOARD) {
+            board_status[i] = BOARDSTATUS_NOK; // Mark path as destroyed
+        }
+    }
+    
+    shark_position=end;
+    
+    return shark_position;
+
+}
+// ----- EX. 5 : shark ------------
+
+
+// ----- EX. 3 : board ------------
+int board_getBoardStatus(int pos)
+{
+    return board_status[pos];
+}
+
+int board_getBoardCoin(int pos)
+{
+    int coin = board_coin[pos];
+    board_coin[pos] = 0;
+    return coin;
+}
+// ----- EX. 3 : board ------------
